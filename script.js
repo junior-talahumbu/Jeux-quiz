46bbbceaf5e20e95e8d569a8e3bb491a3003e767
TALAHUMBU <talahumbujunior@gmail.com>
2026-01-31 18:06:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let questions = [];
 let index = 0;
 let score = 0;
 let timer;
-let timeLeft = 10;
+let timeLeft = 15;
 
 const soundCorrect = new Audio("sounds/correct.mp3");
 const soundWrong = new Audio("sounds/wrong.mp3");
@@ -100,7 +100,7 @@ function next() {
 }
 
 function startTimer() {
-  timeLeft = 10;
+  timeLeft = 15;
   document.getElementById("timer").textContent = `⏱️ ${timeLeft}`;
   timer = setInterval(() => {
     timeLeft--;
