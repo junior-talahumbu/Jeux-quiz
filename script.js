a6b0f8b92650af5fb1fb724d0da91f506da9996f
TALAHUMBU <talahumbujunior@gmail.com>
2026-01-31 18:40:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ function startQuiz() {
 function showQuestion() {
   resetTimer();
   const q = questions[index];
-  document.getElementById("domain").textContent = "📚 " + q.domain;
   document.getElementById("question").textContent = q.question;
   document.getElementById("score").textContent = `Score : ${score}`;
 
